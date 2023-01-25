4da45a573a9361bcc879ca4ee800b355a247ba7d
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-01-25 16:41:35 +0800

    Update 小羊羊生日登入頁面.js

--- 小羊羊生日登入頁面.js
+++ 小羊羊生日登入頁面.js
@@ -1,6 +1,6 @@
 function password(pwd)
         {
-        if(pwd!="寶貝小羊羊生日快樂5201314")
+        if(pwd!="smallsheephappybirthday")
         {
         location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E5%AF%86%E7%A2%BC%E8%BC%B8%E5%85%A5%E9%8C%AF%E8%AA%A4.html")
         }

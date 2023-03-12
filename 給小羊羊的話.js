9a613dd4e0e1ede424e8753cca478b2dd7e0a41d
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-03-12 20:52:24 +0800

    Update 給小羊羊的話.js

--- 給小羊羊的話.js
+++ 給小羊羊的話.js
@@ -1,10 +1,10 @@
 function bb() {
-    location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%878.html")
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E7%B5%A6%E5%B0%8F%E7%BE%8A%E7%BE%8A%E5%A7%90%E5%A7%90%E7%9A%84%E8%A9%B1")
 }
 function mm() {
     location.href=("https://whitecatmeo.github.io/catcatmeowmeow/ee/%E6%9C%80%E5%BE%8C%E4%B8%80%E9%A0%81.html")
 }
 /*
-bb生日影片8
+bb給小羊羊姐姐的話❤-音樂開啟教學
 mm最後一頁
 */

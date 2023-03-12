ad16ffa6433283a15008ecc8d1b7375d735dfbd8
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-03-12 21:04:36 +0800

    Update 給小羊羊的話.js

--- 給小羊羊的話.js
+++ 給小羊羊的話.js
@@ -1,5 +1,5 @@
 function bb() {
-    location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E7%B5%A6%E5%B0%8F%E7%BE%8A%E7%BE%8A%E5%A7%90%E5%A7%90%E7%9A%84%E8%A9%B1")
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E6%95%99%E5%AD%B8")
 }
 function mm() {
     location.href=("https://whitecatmeo.github.io/catcatmeowmeow/ee/%E6%9C%80%E5%BE%8C%E4%B8%80%E9%A0%81.html")

